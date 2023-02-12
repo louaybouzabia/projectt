67ff2947d4731e471a1c7a1b15076d5676b78499
louaybouzabia <louaybouzabia@gmail.com>
2023-02-12 09:44:52 +0100

    project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Formm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+
+        <h1>Objective</h1>
+             <h3 className='first'>In this checkpoint, we are going to make our first contact with React.</h3>
+             
+             <hr/>
+             <hr/>
+
+          <h1> Instructions </h1> 
+          <h3 className='first'>Let’s start playing around with React.</h3>
+          <ol>
+            <li>Create a new React project.</li>
+            <li>Add bootstrap to your project.</li>
+            <li>Try to create a form using bootstrap.</li>
+            <li>In case you meet any problems, take a look at these <a href = "https://reactjs.org/docs/getting-started.html">readings</a>  about React..</li>
+            <li>Create a layout for the website.</li>
+            <li> And Be creative! </li>
+          </ol>
+           
+             
+          <h3 className='first'>Before you start with react, you should follow these steps:</h3>
+          <ol>
+            <li>Install Node JS.</li>
+            <li>Install Visual Studio Code.</li>
+            <li>Create a folder and access it using the command line.</li>
+            <li>Type “ code .” to open the folder in vscode.</li>
+            
+          </ol>
+        <Form></Form>                   
+      
+
     </div>
+  
   );
 }
 
---
+++ src/BasicExemple.js
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">REACT JS Checkpoint</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link href="#">History</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
